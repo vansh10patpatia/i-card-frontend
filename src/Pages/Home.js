ce4111d71facbc6819e1bb5bfba5ed0d8bccd1e3
Vansh Patpatia <vanshpatpatia@Vanshs-MacBook-Air.local>
2022-02-24 02:42:58 +0530

    ui changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,110 +1,109 @@
-import React,{useState,useRef} from 'react'
-import Navbar from "../Components/Navbar"
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import CircularProgress from '@mui/material/CircularProgress';
-import { green } from '@mui/material/colors';
-import { styled } from '@mui/material/styles';
-import Eraimage from "../assets/graphicera.png"
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import domtoimage from 'dom-to-image';
-import {useSelector} from 'react-redux';
+import React, { useState, useRef } from "react";
+import Navbar from "../Components/Navbar";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+// import Grid from "@mui/material/Grid";
+import CircularProgress from "@mui/material/CircularProgress";
+import { green } from "@mui/material/colors";
+import { styled } from "@mui/material/styles";
+import Eraimage from "../assets/graphicera.png";
+// import Card from "@mui/material/Card";
+// import CardContent from "@mui/material/CardContent";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import domtoimage from "dom-to-image";
+import { useSelector } from "react-redux";
 
+const Input = styled("input")({
+  display: "none",
+});
 
+function Form(props) {
+  const userDetails = useSelector((state) => state.user.userDetails);
 
+  const [name, setName] = useState(userDetails?.name);
+  const [email, setEmail] = useState(userDetails?.email);
+  const [phno, setPhno] = useState(userDetails?.phno);
+  const [studentId, setStudentId] = useState(userDetails?.userid);
+  const [session, setSession] = useState(userDetails?.session);
+  const [image, setImage] = useState(require("../assets/avtar.png"));
+  const [imageName, setImageName] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
 
+  const buttonSx = {
+    ...(success && {
+      bgcolor: green[500],
+      "&:hover": {
+        bgcolor: green[700],
+      },
+    }),
+  };
 
+  const printRef = useRef();
 
+  function downloadImage() {
+    setSuccess(false);
+    setLoading(true);
+    domtoimage
+      .toBlob(document.getElementById("i-card-container"))
+      .then(function (blob) {
+        require("downloadjs")(blob, "i-card.png");
+        setSuccess(true);
+        setLoading(false);
+      });
+  }
 
-const Input = styled('input')({
-    display: 'none',
-  });
+  function onSelectFile(e) {
+    setImageName("");
+    setImageName(e.target.files[0].name);
+    setImage(URL.createObjectURL(e.target.files[0]));
+    console.log(URL.createObjectURL(e.target.files[0]));
+  }
 
-
-function Form(props){
-
-    const userDetails = useSelector((state)=>state.user.userDetails);
-
-    const [name,setName] = useState(userDetails?.name)
-    const [email,setEmail] = useState(userDetails?.email);
-    const [phno,setPhno] = useState(userDetails?.phno);
-    const [studentId,setStudentId] = useState(userDetails?.userid);
-    const [session,setSession] = useState(userDetails?.session);
-    const [image,setImage] = useState(require('../assets/avtar.png'));
-    const [imageName,setImageName] = useState('');
-    const [loading,setLoading] = useState(false)
-    const [success, setSuccess] = useState(false);
-
-    const buttonSx = {
-        ...(success && {
-          bgcolor: green[500],
-          '&:hover': {
-            bgcolor: green[700],
-          },
-        }),
-      };
-    
-
-    const printRef = useRef();
-
-    function downloadImage(){
-        setSuccess(false);
-        setLoading(true);
-        domtoimage.toBlob(document.getElementById('i-card-container'))
-            .then(function (blob) {
-                require("downloadjs")(blob, 'i-card.png');
-                setSuccess(true);
-                setLoading(false);
-            });
-    }
-
-    function onSelectFile(e){
-        setImageName('')
-        setImageName(e.target.files[0].name)
-        setImage(URL.createObjectURL(e.target.files[0]))
-        console.log(URL.createObjectURL(e.target.files[0]));
-    }
-
-    return(
-        <>
-           <Navbar name={name}/>
-           <Box className='Box1'>
-                <Typography className='heading-name' variant="h4" component="h3">
-                    Fill in the details
-                </Typography>
-            </Box>
-            <div className="home-page-all-containers">
-                <div className="i-card-girl-image">
-                    <img src={require('../assets/dumyimage.png')} className='girl-card-image' alt="" />
+  return (
+    <>
+      <Navbar name={name} />
+      <Box className="Box1">
+        <Typography className="heading-name" variant="h4" component="h3">
+          Fill in the details
+        </Typography>
+      </Box>
+      <div className="home-page-all-containers">
+        <div className="i-card-girl-image">
+          <img
+            src={require("../assets/dumyimage.png")}
+            className="girl-card-image"
+            alt=""
+          />
+        </div>
+        <div className="main-containers-for-page">
+          <div className="i-card-wrapper">
+            <div
+              className="id-card-container"
+              id="i-card-container"
+              ref={printRef}
+            >
+              <div className="id-card-img-container">
+                <img src={Eraimage} alt="" />
+                <div className="avatar-container">
+                  <img src={image} alt="" />
                 </div>
-                <div className="main-containers-for-page">
-                    <div className="i-card-wrapper">
-
-                        <div className="id-card-container" id='i-card-container' ref={printRef}>
-                            <div className="id-card-img-container">
-                                <img src={Eraimage} alt="" />
-                                <div className="avatar-container">
-                                    <img src={image} alt="" />
-                                </div>
-                            </div>
-                            <div className="top-text-div" >
-                                <img src={require('../assets/icon.png')} alt="" />
-                                <p className='geu-name'>Graphic Era Deemed to be University</p>
-                            </div>
-                            <div className="id-card-text-container">
-                                <h1>{name}</h1>
-                                <p>{phno}</p>
-                                <p> Id : {studentId} </p>
-                                <p>{email}</p>
-                                <p>Session : {session}</p>
-                            </div>
-                        </div>
-                    </div>
-                    <Card variant="outlined" className="admin-login-card">
+              </div>
+              <div className="top-text-div">
+                <img src={require("../assets/icon.png")} alt="" />
+                <p className="geu-name">Graphic Era Deemed to be University</p>
+              </div>
+              <div className="id-card-text-container">
+                <h1>{name}</h1>
+                <p>{phno}</p>
+                <p> Id : {studentId} </p>
+                <p>{email}</p>
+                <p>Session : {session}</p>
+              </div>
+            </div>
+          </div>
+          {/* <Card variant="outlined" className="admin-login-card">
                         <CardContent>
                             <Typography variant="h4" color="text.secondary" className="admin-login-welcome" style={{fontWeight: 600}} gutterBottom>
                                 Graphic Era I card
@@ -194,11 +193,85 @@ function Form(props){
                                 </Box>
                             </Typography>
                         </CardContent>
-                    </Card>
-                </div>
+                    </Card> */}
+          <div className="data-input-card-container">
+            <h1>Graphic Era I card</h1>
+            <div className="input-feild-container">
+              <div className="textOnInput"> 
+                <TextField id="outlined-basic" label="Name" className="login-inputs" variant="outlined" placeholder="Enter your name..."  focused 
+                                        value={name}
+                                        onChange={(event)=>{setName(event.target.value)}}
+                                        /> 
+              </div>
+              <div className="textOnInput"> 
+                <TextField id="outlined-basic" label="Email" variant="outlined" className="login-inputs" placeholder="Type your email..."  focused 
+                                        value={email}
+                                        onChange={(event)=>{setEmail(event.target.value)}}
+                                        />
+              </div>
+              <div className="textOnInput"> 
+                                        <TextField id="outlined-basic" label="Student Id" className="login-inputs" variant="outlined" placeholder="Enter your student id..."  focused 
+                                        value={studentId}
+                                        onChange={(event)=>{setStudentId(event.target.value)}}
+                                        />
+              </div>
+              <div className="textOnInput"> 
+                                        <TextField id="outlined-basic" label="Phone" variant="outlined" className="login-inputs" placeholder="Type your number..."  focused 
+                                        onChange={(event)=>{setPhno(event.target.value)}}
+                                        value={phno}
+                                        />
+              </div>
+              <div className="textOnInput">  
+                                        <TextField id="outlined-basic" label="Session" className="login-inputs" variant="outlined" placeholder="Eg. 2020-24"  focused 
+                                        value={session}
+                                        onChange={(event)=>{setSession(event.target.value)}}
+                                        />
+              </div>
+              <div className="textOnInput">  
+                <Typography variant="p" component="div" className="login-input-container">
+                                        <label htmlFor="contained-button-file">
+                                            <Input accept="image/*" id="contained-button-file" multiple type="file" onChange={(event)=>onSelectFile(event)}/>
+                                            <Button variant="contained" 
+                                             component="span">
+                                                Upload Image
+                                            </Button>
+                                        </label>
+                                        <br />
+                                        {imageName}
+                                    </Typography>
+              </div>
             </div>
-        </>
+            <div className="input-download-btn">
 
-    )
+            <Box sx={{ m: 1, position: 'relative' }}>
+                                    <Button
+                                        variant="contained"
+                                        className="continue-button"
+                                        sx={buttonSx}
+                                        disabled={loading}
+                                        onClick={()=>downloadImage()} 
+                                    >
+                                        Download
+                                    </Button>
+                                    {loading && (
+                                    <CircularProgress
+                                        size={24}
+                                        sx={{
+                                        color: green[500],
+                                        position: 'absolute',
+                                        top: '50%',
+                                        left: '50%',
+                                        marginTop: '-12px',
+                                        marginLeft: '-12px',
+                                        }}
+                                    />
+                                    )}
+                                </Box>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
-export default Form
+export default Form;
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,67 +1,74 @@
-import React,{useState} from 'react';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import TextField from '@mui/material/TextField';
-import CircularProgress from '@mui/material/CircularProgress';
-import {useNavigate} from 'react-router-dom';
-import {loginAdmin} from '../APIs/login';
-import {useDispatch} from 'react-redux';
-import { SET_AUTH_STATUS, SET_USER_DETAILS, SET_ACCESS_TOKEN } from '../Reducers/types';
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+// import Card from "@mui/material/Card";
+// import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+// import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import CircularProgress from "@mui/material/CircularProgress";
+import { useNavigate } from "react-router-dom";
+import { loginAdmin } from "../APIs/login";
+import { useDispatch } from "react-redux";
+import {
+  SET_AUTH_STATUS,
+  SET_USER_DETAILS,
+  SET_ACCESS_TOKEN,
+} from "../Reducers/types";
 
-export default function Login(props){
+export default function Login(props) {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
-    
-    const navigate = useNavigate();
-    const [passwordError , setPasswordError] = useState(false);
-    const [passwordErrorText , setPasswordErrorText] = useState("Default");
-    const [useridError , setuseridError] = useState(false);
-    const [useridErrorText , setuseridErrorText] = useState("Default");
-    // const [loading,setLoading] = useState(false)
-    const {loading,setLoading} = props;
+  const navigate = useNavigate();
+  const [passwordError, setPasswordError] = useState(false);
+  const [passwordErrorText, setPasswordErrorText] = useState("Default");
+  const [useridError, setuseridError] = useState(false);
+  const [useridErrorText, setuseridErrorText] = useState("Default");
+  // const [loading,setLoading] = useState(false)
+  const { loading, setLoading } = props;
 
-    const [userid,setuserid] = useState('');
-    const [password,setPassword] = useState('');
+  const [userid, setuserid] = useState("");
+  const [password, setPassword] = useState("");
 
-    function loginStudent(){
-        setuseridError(false);
-        setPasswordError(false);
-        setLoading(true);
-        loginAdmin({userid, password})
-                .then((response) =>{
-                    if(response){
-                        localStorage.setItem('accessToken', response.data.accessToken);
-                        dispatch({ type: SET_AUTH_STATUS, payload: { authStatus: true } });
-                        dispatch({ type: SET_USER_DETAILS, payload: { userDetails: response.data } });
-                        dispatch({ type: SET_ACCESS_TOKEN, payload: { accessToken: response.data.accessToken } });
-                        navigate("/generate")
-                    }
-                    else{
-                        setPasswordError(true);
-                        setPasswordErrorText("Invalide Credentials");
-                        setuseridError(true);
-                        setuseridErrorText("Invalid Credentials");
-                    }
-                    setLoading(false)
-                })
-                .catch((err) => {
-                    console.log(err);
-                    setLoading(false)
-                })
-    }
+  function loginStudent() {
+    setuseridError(false);
+    setPasswordError(false);
+    setLoading(true);
+    loginAdmin({ userid, password })
+      .then((response) => {
+        if (response) {
+          localStorage.setItem("accessToken", response.data.accessToken);
+          dispatch({ type: SET_AUTH_STATUS, payload: { authStatus: true } });
+          dispatch({
+            type: SET_USER_DETAILS,
+            payload: { userDetails: response.data },
+          });
+          dispatch({
+            type: SET_ACCESS_TOKEN,
+            payload: { accessToken: response.data.accessToken },
+          });
+          navigate("/generate");
+        } else {
+          setPasswordError(true);
+          setPasswordErrorText("Invalide Credentials");
+          setuseridError(true);
+          setuseridErrorText("Invalid Credentials");
+        }
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  }
 
-    return (
-        <>
-        <div className="login-page-container">
-            <div className="login-page-img-container">
-                <img src={require('../assets/loginImage.png')} alt="login"  />
-            </div>
+  return (
+    <>
+      <div className="login-page-container">
+        <div className="login-page-img-container">
+          <img src={require("../assets/loginImage.png")} alt="login" />
+        </div>
         <div className="login-page-card-container">
-            
-        <Card variant="outlined" className="admin-login-screen-card">
+          {/* <Card variant="outlined" className="admin-login-screen-card">
                         <CardContent>
                             <Typography variant="h4" color="text.secondary" className="admin-login-welcome" style={{fontWeight: 600}} gutterBottom>
                                 Hey, Welcome Back!
@@ -106,9 +113,78 @@ export default function Login(props){
                                 </Box>
                             </Typography>
                         </CardContent>
-                    </Card>
+                    </Card> */}
+
+                        <div className="data-input-card-container login-input-container-login">
+                            <h1>Hey, Welcome Back!</h1>
+                            <h2> Log in to continue</h2>
+                            <div className="input-feild-container">
+                                <div className="textOnInput">
+
+                            <TextField id="outlined-basic" label="User-Id" className="login-inputs" variant="outlined" placeholder="Enter your user-id..."  focused 
+                                error={useridError} helperText={useridError?(useridErrorText):(null)}
+                                onChange={(event)=>{setuserid(event.target.value)}}
+                                />
                                 </div>
+                                <div className="textOnInput">
+
+                                 <TextField id="outlined-basic" type="password" label="Password" variant="outlined" className="login-inputs" placeholder="Type your password"  focused error={passwordError} helperText={passwordError?(passwordErrorText):(null)}
+                                onChange={(event)=>{setPassword(event.target.value)}}/>
                                 </div>
-        </>
-    );
+                            </div>
+                            <Box sx={{ m: 1, position: 'relative' }}>
+                                    <Button
+                                        variant="contained"
+                                        className="continue-button"
+                                        disabled={loading}
+                                        onClick={()=>loginStudent()}
+                                        >
+                                        Continue
+                                    </Button>
+                                    {loading && (
+                                    <CircularProgress
+                                        size={24}
+                                        sx={{
+                                            position: 'absolute',
+                                            top: '50%',
+                                            left: '50%',
+                                            marginTop: '-12px',
+                                            marginLeft: '-12px',
+                                        }}
+                                    />
+                                    )}
+                                </Box>
+                            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+      </div>
+    </>
+  );
 }
