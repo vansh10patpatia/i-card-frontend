22df382dc6f87e8c0d80096555b52c999cdbbbaa
Vansh Patpatia <68200019+vansh10patpatia@users.noreply.github.com>
2022-03-07 23:23:18 +0530

    added localhost url in config

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
 
-export const apiUrl = "https://i-card-generator.herokuapp.com/v1"
+// export const apiUrl = "https://i-card-generator.herokuapp.com/v1"
+export const apiUrl = "http://localhost:3000/v1"
